372542ab14df0047b6c4349ddf58a721b05a73fe
Jmock27 <157052628+Jmock27@users.noreply.github.com>
2024-04-29 13:00:23 -0500

    Scene Transitions

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,12 @@
 extends Control
 
-func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://gameLevel.tscn")
+var gameLevel = preload("res://gameLevel.tscn")
+
+
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://gameLevel.tscn")
