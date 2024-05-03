754fe7a439c2dfff46d0c7b2eab22250cdbdb105
Jmock27 <jmock@murraystate.edu>
2024-05-02 21:30:12 -0500

    c

--- enemy.gd
+++ enemy.gd
@@ -1,27 +1,13 @@
 extends CharacterBody2D
 
-@export var speed = 99
-var direction = -1;
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	velocity.x = speed * direction
+func _physics_process(delta):
+	velocity.y += gravity * delta
 	
-	if(direction == -1):
-		$AnimatedSprite2D.flip_h = false
-	else:
-		$AnimatedSprite2D.flip_h = true
-
 	move_and_slide()
-	
-	if $RayCast2D.is_colliding()==false:
-		direction *= -1
-		$RayCast2D.position.x *= -1
 
-func _on_collision_checker_body_entered(body):
-	print("_on_collision_checker_body_entered")
-	body.hit()
+
+func _on_player_detection_body_entered(body):
+	if body.name == "pirate":
+		print("pirate")
